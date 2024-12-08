cfa46639f0dd10a33a766393c3719201b6671864
miya.ong <miya.ong@kakaocorp.com>
2024-12-08 21:28:46 +0900

    feat add logo and title

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -14,13 +14,13 @@ export default function HomePage() {
         <ChatbotUISVG theme={theme === "dark" ? "dark" : "light"} scale={0.3} />
       </div>
 
-      <div className="mt-2 text-4xl font-bold">Chatbot UI</div>
+      <div className="mt-2 text-4xl font-bold">똑똑한 고양이 미로</div>
 
       <Link
         className="mt-4 flex w-[200px] items-center justify-center rounded-md bg-blue-500 p-2 font-semibold"
         href="/login"
       >
-        Start Chatting
+        미로에게 물어보세요.
         <IconArrowRight className="ml-1" size={20} />
       </Link>
     </div>
